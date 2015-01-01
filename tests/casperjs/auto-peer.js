bf581b7e30173bfb8bc035ae4f381939434244dc
Jan Nicklas <j.nicklas@me.com>
2015-01-01 22:19:30 +0100

    Add send-self tests

--- tests/casperjs/auto-peer.js
+++ tests/casperjs/auto-peer.js
@@ -1,21 +1,21 @@
 var casper = require('casper').create({
-  logLevel: "info",              // Only "info" level messages will be logged
+  logLevel: "info"
 });
 /* global casper */
-casper.test.begin("auto peer", 6, function (test) {
+casper.test.begin("auto peer", 9, function (test) {
 
   // Initialize the testing environment
   casper
-    .start('http://127.0.0.1:3001/', function() {
-      return this.waitForSelector('body.peers-ready', function(){
+    .start('http://127.0.0.1:3001/', function () {
+      return this.waitForSelector('body.peers-ready', function () {
       });
     }, 10000);
 
   casper.wait(5000);
 
   // Assert that all peers are up and running
-  casper.then(function() {
-    var runningPeerCount = casper.evaluate(function(){
+  casper.then(function () {
+    var runningPeerCount = casper.evaluate(function () {
       return window.testingEnvironments.length;
     });
     test.assertEquals(runningPeerCount, 3, 'Three iframes registered their testing environment');
@@ -25,7 +25,7 @@ casper.test.begin("auto peer", 6, function (test) {
   casper.then(function () {
     var messages = casper.evaluate(function () {
       var messages = [];
-      window.testingEnvironments.forEach(function(testingEnvironment){
+      window.testingEnvironments.forEach(function (testingEnvironment) {
         messages.concat(testingEnvironment.dataReceived);
       });
       return messages;
@@ -47,17 +47,42 @@ casper.test.begin("auto peer", 6, function (test) {
     // Wait for 100ms
     casper.wait(100);
 
-    casper.then(function(){
-    // Test
-    test.assertEvalEquals(function(){
-      return window.testingEnvironments[0].dataReceived;
-    }, [], 'Peer one did not receive its own message');
-    test.assertEvalEquals(function(){
-      return window.testingEnvironments[1].dataReceived;
-    }, ['hello-peers'], 'Peer two received the message');
-    test.assertEvalEquals(function(){
-      return window.testingEnvironments[2].dataReceived;
-    }, ['hello-peers'], 'Peer three received the message');
+    casper.then(function () {
+      // Test
+      test.assertEvalEquals(function () {
+        return window.testingEnvironments[0].dataReceived;
+      }, [], 'Peer one did not receive its own message');
+      test.assertEvalEquals(function () {
+        return window.testingEnvironments[1].dataReceived;
+      }, ['hello-peers'], 'Peer two received the message');
+      test.assertEvalEquals(function () {
+        return window.testingEnvironments[2].dataReceived;
+      }, ['hello-peers'], 'Peer three received the message');
+
+    });
+  });
+
+  // Assert that the send self option works
+  casper.then(function () {
+    // Send
+    casper.evaluate(function () {
+      window.clearData();
+      window.testingEnvironments[0].autoPeer.send('hello-peers-send-self', true);
+    });
+    // Wait for 100ms
+    casper.wait(100);
+
+    casper.then(function () {
+      // Test
+      test.assertEvalEquals(function () {
+        return window.testingEnvironments[0].dataReceived;
+      }, ['hello-peers-send-self'], 'Send-self: Peer one did receive its own message');
+      test.assertEvalEquals(function () {
+        return window.testingEnvironments[1].dataReceived;
+      }, ['hello-peers-send-self'], 'Send-self: Peer two received the message');
+      test.assertEvalEquals(function () {
+        return window.testingEnvironments[2].dataReceived;
+      }, ['hello-peers-send-self'], 'Send-self: Peer three received the message');
 
     });
   });
